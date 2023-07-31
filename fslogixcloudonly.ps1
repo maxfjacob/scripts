9625b09c7bf11a126a4748e8650acdfa45b0c9da
maxfjacob <54584792+maxfjacob@users.noreply.github.com>
2023-07-31 12:20:40 +0200

    Add files via upload

---
+++ fslogixcloudonly.ps1
@@ -0,0 +1,30 @@
+﻿Write-Host "Configure FSLogix"
+
+$fileServer="avdstgusrprofilemfj.file.core.windows.net"
+$user="localhost\avdstgusrprofilemfj"
+$profileShare="\\$($fileServer)\fslogix"
+$secret="Wfe0zmzkyDPBWtU69ac9EFs5lDTEvmlheiZwrYq/yM0sMvTFarS5oZbv0wvUdxL5Nb/RWAWutRY4+AStMsdkPA=="
+
+ # Save the password so the drive will persist on reboot
+ cmd.exe /c "cmdkey /add:$fileServer /user:$($user) /pass:$($secret)"
+
+
+New-Item -Path "HKLM:\SOFTWARE" -Name "FSLogix" -ErrorAction Ignore
+New-Item -Path "HKLM:\SOFTWARE\FSLogix" -Name "Profiles" -ErrorAction Ignore
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "Enabled" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VHDLocations" -Value $profileShare -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "ConcurrentUserSessions" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "DeleteLocalProfileWhenVHDShouldApply" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "FlipFlopProfileDirectoryName" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "IsDynamic" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "KeepLocalDir" -Value 0 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "ProfileType" -Value 0 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "SizeInMBs" -Value 40000 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VolumeType" -Value "VHDX" -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "AccessNetworkAsComputerObject" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LsaCfgFlags" -Value 0 -force  # Only needed for Windows 11 22H2
+
+# Disable Windows Defender Credential Guard (only needed for Windows 11 22H2)
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LsaCfgFlags" -Value 0 -force
+
+Write-Host "The script has finished"
